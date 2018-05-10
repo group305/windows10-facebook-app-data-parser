b3aeb442125c67e07814ec60f297dc96b769ae35
mcoates1 <marcus.coates12@gmail.com>
2018-05-09 22:17:20 -0400

    Bug fixes to module not loading.

--- facebookDataSourceIngestModule.py
+++ facebookDataSourceIngestModule.py
@@ -42,7 +42,7 @@ from org.sleuthkit.datamodel import BlackboardArtifact
 from org.sleuthkit.datamodel import BlackboardAttribute
 from org.sleuthkit.autopsy.ingest import IngestModule
 from org.sleuthkit.autopsy.ingest.IngestModule import IngestModuleException
-from org.sleuthkit.autopsy.ingest import DBIngestModule
+from org.sleuthkit.autopsy.ingest import DataSourceIngestModule
 from org.sleuthkit.autopsy.ingest import FileIngestModule
 from org.sleuthkit.autopsy.ingest import IngestModuleFactoryAdapter
 from org.sleuthkit.autopsy.ingest import IngestMessage
@@ -57,7 +57,7 @@ from org.sleuthkit.autopsy.casemodule.services import Blackboard
 # Factory that defines the name and details of the module and allows Autopsy
 # to create instances of the modules that will do the analysis.
 # TODO: Rename this to something more specific. Search and replace for it because it is used a few times
-class FacebookDBIngestModuleFactory(IngestModuleFactoryAdapter):
+class SampleJythonDataSourceIngestModuleFactory(IngestModuleFactoryAdapter):
 
     # TODO: give it a unique name.  Will be shown in module list, logs, etc.
     moduleName = "Facebook Data Source Module"
@@ -72,19 +72,19 @@ class FacebookDBIngestModuleFactory(IngestModuleFactoryAdapter):
     def getModuleVersionNumber(self):
         return "1.0"
 
-    def isDBIngestModuleFactory(self):
+    def isDataSourceIngestModuleFactory(self):
         return True
 
-    def createDBIngestModule(self, ingestOptions):
+    def createDataSourceIngestModule(self, ingestOptions):
         # TODO: Change the class name to the name you'll make below
-        return FacebookDBIngestModule()
+        return SampleJythonDataSourceIngestModule()
 
 
 # Data Source-level ingest module.  One gets created per data source.
 # TODO: Rename this to something more specific. Could just remove "Factory" from above name.
-class FacebookDBIngestModule(DBIngestModule):
+class SampleJythonDataSourceIngestModule(DataSourceIngestModule):
 
-    _logger = Logger.getLogger(FacebookDBIngestModuleFactory.moduleName)
+    _logger = Logger.getLogger(SampleJythonDataSourceIngestModuleFactory.moduleName)
 
     def log(self, level, msg):
         self._logger.logp(level, self.__class__.__name__, inspect.stack()[1][3], msg)
@@ -103,11 +103,68 @@ class FacebookDBIngestModule(DBIngestModule):
         self.context = context
 
     # Where the analysis is done.
-    # The 'DB' object being passed in is of type org.sleuthkit.datamodel.Content.
+    # The 'dataSource' object being passed in is of type org.sleuthkit.datamodel.Content.
     # See: http://www.sleuthkit.org/sleuthkit/docs/jni-docs/4.4/interfaceorg_1_1sleuthkit_1_1datamodel_1_1_content.html
-    # 'progressBar' is of type org.sleuthkit.autopsy.ingest.DBIngestModuleProgress
+    # 'progressBar' is of type org.sleuthkit.autopsy.ingest.DataSourceIngestModuleProgress
     # See: http://sleuthkit.org/autopsy/docs/api-docs/4.4/classorg_1_1sleuthkit_1_1autopsy_1_1ingest_1_1_data_source_ingest_module_progress.html
     # TODO: Add your analysis code in here.
-    def process(self, DB, progressBar):
+    def process(self, dataSource, progressBar):
 
-        return None;
+        # we don't know how much work there is yet
+        progressBar.switchToIndeterminate()
+
+        # Use blackboard class to index blackboard artifacts for keyword search
+        blackboard = Case.getCurrentCase().getServices().getBlackboard()
+
+        # For our example, we will use FileManager to get all
+        # files with the word "test"
+        # in the name and then count and read them
+        # FileManager API: http://sleuthkit.org/autopsy/docs/api-docs/4.4/classorg_1_1sleuthkit_1_1autopsy_1_1casemodule_1_1services_1_1_file_manager.html
+        fileManager = Case.getCurrentCase().getServices().getFileManager()
+        files = fileManager.findFiles(dataSource, "%test%")
+
+        numFiles = len(files)
+        self.log(Level.INFO, "found " + str(numFiles) + " files")
+        progressBar.switchToDeterminate(numFiles)
+        fileCount = 0
+        for file in files:
+
+            # Check if the user pressed cancel while we were busy
+            if self.context.isJobCancelled():
+                return IngestModule.ProcessResult.OK
+
+            self.log(Level.INFO, "Processing file: " + file.getName())
+            fileCount += 1
+
+            # Make an artifact on the blackboard.  TSK_INTERESTING_FILE_HIT is a generic type of
+            # artfiact.  Refer to the developer docs for other examples.
+            art = file.newArtifact(BlackboardArtifact.ARTIFACT_TYPE.TSK_INTERESTING_FILE_HIT)
+            att = BlackboardAttribute(BlackboardAttribute.ATTRIBUTE_TYPE.TSK_SET_NAME, SampleJythonDataSourceIngestModuleFactory.moduleName, "Test file")
+            art.addAttribute(att)
+
+            try:
+                # index the artifact for keyword search
+                blackboard.indexArtifact(art)
+            except Blackboard.BlackboardException as e:
+                self.log(Level.SEVERE, "Error indexing artifact " + art.getDisplayName())
+
+            # To further the example, this code will read the contents of the file and count the number of bytes
+            inputStream = ReadContentInputStream(file)
+            buffer = jarray.zeros(1024, "b")
+            totLen = 0
+            readLen = inputStream.read(buffer)
+            while (readLen != -1):
+                totLen = totLen + readLen
+                readLen = inputStream.read(buffer)
+
+
+            # Update the progress bar
+            progressBar.progress(fileCount)
+
+
+        #Post a message to the ingest messages in box.
+        message = IngestMessage.createMessage(IngestMessage.MessageType.DATA,
+            "Sample Jython Data Source Ingest Module", "Found %d files" % fileCount)
+        IngestServices.getInstance().postMessage(message)
+
+        return IngestModule.ProcessResult.OK
